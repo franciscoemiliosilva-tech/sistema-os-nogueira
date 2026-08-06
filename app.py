7f71051dad707195dbe69ddb3c8126c932a29240
franciscoemiliosilva-tech <franciscoemilio.silva@gmail.com>
2026-08-06 16:01:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -415,11 +415,12 @@ if arquivo_excel and arquivo_csv_3d:
             if cat not in relatorio: relatorio[cat] = {}
             
             # Contagem de Espumas Especiais para Lista de Compras
+            # NOVA REGRA: O padrão é P. Só será G se tiver a palavra GRANDE ou G na frente.
             if "BOXE" in nome_upper or re.search(r'\bBOX\b', nome_upper):
-                if " P" in nome_upper or "-P" in nome_upper or "PEQ" in nome_upper or " P " in nome_upper:
-                    espumas_calc["ESPUMA CILINDRICA 20X20X60CM - P"] += 1
-                else:
+                if " G" in nome_upper or "-G" in nome_upper or "GRAND" in nome_upper or " G " in nome_upper:
                     espumas_calc["ESPUMA CILINDRICA 26X26X60CM - G"] += 1
+                else:
+                    espumas_calc["ESPUMA CILINDRICA 20X20X60CM - P"] += 1
                     
             if "CORCOVA" in nome_upper:
                 if "TRIANG" in nome_upper:
